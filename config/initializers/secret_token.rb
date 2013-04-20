f15de77ec082d433eda66a72dfdaa63ce65b4308
Keith <keithmiketom@gmail.com>
2013-04-21 00:49:17 +0100

    ajax request lab with basic styling

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ajax03::Application.config.secret_token = 'b7672a26969f7d4c4680c5421c8365b33a1ac6f98503635fa9f2ed9f06b5c3ef4423d7e3fa27039051227ac52d1cffe41887d44ba4008e2d92dabc4c1615d912'
